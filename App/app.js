b56d9f69b5018120bcae6aa7db0622b2f10c9408
penda joseph <johakimazix4@gmail.com>
2022-07-04 05:09:22 +0200

    allowing github pages

--- App/app.js
+++ App/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const app = express()
+const cors = require('cors')
 const authRouter = require("../routers/Auth")
 const mediaRouter = require("../routers/Media")
 mongoose.connect(env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('Connexion à MongoDB réussie !')).catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -10,8 +11,11 @@ mongoose.connect(env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).
 app.use(express.json());
 app.use(cookieParser());
 app.use("/public",express.static("public"))
+app.use(cors({
+    origin:'*',
+}))
 app.use((req, res, next) => { /*todo : general middleware ; it's applied to every request*/
-    res.setHeader('Access-Control-Allow-Origin', "http://localhost:8887");
+    // res.setHeader('Access-Control-Allow-Origin', "http://localhost:8887");
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
