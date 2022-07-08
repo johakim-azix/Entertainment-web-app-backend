39a3a5ed09de7bb45383d7d24f870147c94166d0
penda joseph <johakimazix4@gmail.com>
2022-07-08 02:52:03 +0200

    fix client urls

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 const DB_NAME = "Entertainment-app-db"
 exports.PORT = 4000
 exports.DB_URL = "mongodb://localhost/"+DB_NAME
-exports.CLIENT_URL = ["www.entertainment-web-app.marlonne-penda.space","entertainment-web-app.marlonne-penda.space"]
+exports.CLIENT_URL = ["http://www.entertainment-web-app.marlonne-penda.space/register","http://entertainment-web-app.marlonne-penda.space"]
