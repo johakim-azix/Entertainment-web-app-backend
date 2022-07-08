154f3b637261b80e4f69052e5558ad8232c779db
penda joseph <johakimazix4@gmail.com>
2022-07-08 03:48:05 +0200

    fix client urls

--- App/app.js
+++ App/app.js
@@ -10,11 +10,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/public",express.static("public"))
 app.use((req, res, next) => { /*todo : general middleware ; it's applied to every request*/
-    // env.CLIENT_URL.forEach(url =>{
-    //     if ( url === req.headers.origin)
-            res.setHeader('Access-Control-Allow-Origin', req.origin);
+    env.CLIENT_URL.forEach(url =>{
+        if ( url === req.headers.origin) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
         // console.log(req.headers)
-    // })
+    })
 
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
