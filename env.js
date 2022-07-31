4cbfa9a65ab6b2298deaf8db8c72c59b80021fcb
penda joseph <johakimazix4@gmail.com>
2022-07-31 03:35:47 +0200

    reviewing the client urls aftter ssl cert subscription

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const DB_NAME = "entertainment-app-db"
 exports.PORT = 4000
 exports.DB_URL = "mongodb://localhost/"+DB_NAME
-exports.CLIENT_URL = ["http://www.entertainment-web-app.marlonne-penda.space","http://entertainment-web-app.marlonne-penda.space"]
+exports.CLIENT_URL = ["https://www.entertainment-web-app.marlonne-penda.space","https://entertainment-web-app.marlonne-penda.space"]
 // exports.CLIENT_URL = ["http://localhost:8080","http://localhost:8887","http://127.0.0.1:8887"]
