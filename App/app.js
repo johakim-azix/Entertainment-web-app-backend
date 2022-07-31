97ff173f49512d9f92f72f850484361e8eb72931
penda joseph <johakimazix4@gmail.com>
2022-07-31 04:19:14 +0200

    fixing cors after ssl cert

--- App/app.js
+++ App/app.js
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/public", express.static("public"))
 app.use((req, res, next) => { /*todo : general middleware ; it's applied to every request*/
-    env.CLIENT_URL.forEach(url => {
-        if (url === req.headers.origin) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    })
+    // env.CLIENT_URL.forEach(url => {
+    //     if (url === req.headers.origin) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    // })
+    res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
